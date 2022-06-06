b8963c8e03c7d7887d7bc93b04a49d76c378fe1f
Lennna <pampam32121@gmail.com>
2022-06-07 02:44:38 +0300

    modified height of card, used constructor fn for storing data and rendering characters

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 'use strict';
 
+function getDiceRollArray(diceCount) {
+    return new Array(diceCount)
+        .fill(0)
+        .map( () => Math.floor(Math.random() * 6) + 1);
+};
+
+function getDiceHtml(diceCount) {
+    return getDiceRollArray(diceCount)
+            .map(num => `<div class="dice">${num}</div>`)
+            .join('');
+};
+
 const hero = {
     id: 'hero',
     name: 'Wizard',
     avatarSrc: './images/wizard.png',
     health: 60,
     diceCount: 3,
-}
+};
 
 const monster = {
     id: 'monster',
@@ -16,32 +28,29 @@ const monster = {
     diceCount: 1,
 };
 
+function Character(data) {
+    this.id = data.id;
+    this.name = data.name;
+    this.avatarSrc = data.avatarSrc;
+    this.health = data.health;
+    this.diceCount = data.diceCount;
 
-function getDiceRollArray(diceCount) {
-    return new Array(diceCount)
-        .fill(0)
-        .map( () => Math.floor(Math.random() * 6) + 1);
+    this.renderCharacter = function() {
+        const diceHtml = getDiceHtml(this.diceCount);
+
+        document.getElementById(this.id).innerHTML = `
+            <div class="character-card">
+                <h4 class="name">${this.name}</h4>
+                <img class="avatar" src="${this.avatarSrc}"/>
+                <p class="health">health: <b> ${this.health} </b></p>
+                <div class="dice-container">${diceHtml}</div>
+            </div>   
+        `;
+    }
 }
 
-function getDiceHtml(diceCount) {
-    return getDiceRollArray(diceCount)
-            .map(num => `<div class="dice">${num}</div>`)
-            .join('');
-}
+const wizard = new Character(hero);
+const orc = new Character(monster);
 
-function renderCharacter({ id, name, avatarSrc, health, diceCount }) {
-
-    const diceHtml = getDiceHtml(diceCount);
-
-    document.getElementById(id).innerHTML = `
-        <div class="character-card">
-            <h4 class="name">${name}</h4>
-            <img class="avatar" src="${avatarSrc}"/>
-            <p class="health">health: <b> ${health} </b></p>
-            <div class="dice-container">${diceHtml}</div>
-        </div>   
-    `
-}
-
-renderCharacter(hero);
-renderCharacter(monster);
+wizard.renderCharacter();
+orc.renderCharacter();
