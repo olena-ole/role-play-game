0eb2259d95bd84b3a6a2ef0e0349d6e8b32db562
Lennna <pampam32121@gmail.com>
2022-06-06 17:23:04 +0300

    renderCharacter fn renders hardcoded dices from array

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 'use strict';
 
-/*
-CHALLENGE
-1. Convert our consts into two objects called 
-"monster" and "hero".
-2. Update the renderCharacter() function so that it accepts 
-a single object "data" as its parameter instead of five string/numbers, 
-reducing the number of arguments to pass in from five to one.
-3. Update the template now each variable is coming from "data".
-4. Update the function call.
-*/
-
 const hero = {
     id: 'hero',
     name: 'Wizard',
     avatarSrc: './images/wizard.png',
     health: 60,
-    diceRoll: 6,
+    diceRoll: [3, 1, 4],
 }
 
 const monster = {
@@ -24,17 +13,20 @@ const monster = {
     name: 'Orc',
     avatarSrc: './images/orc.png',
     health: 10,
-    diceRoll: 4,
+    diceRoll: [2],
 };
 
 
-function renderCharacter({id, name, avatarSrc, health, diceRoll}) {
+function renderCharacter({ id, name, avatarSrc, health, diceRoll }) {
+
+    const diceHtml = diceRoll.map(dice => `<div class="dice">${dice}</div>`).join('');
+
     document.getElementById(id).innerHTML = `
         <div class="character-card">
             <h4 class="name">${name}</h4>
             <img class="avatar" src="${avatarSrc}"/>
             <p class="health">health: <b> ${health} </b></p>
-            <div class="dice-container"><div class="dice">${diceRoll}</div></div>
+            <div class="dice-container">${diceHtml}</div>
         </div>   
     `
 }
