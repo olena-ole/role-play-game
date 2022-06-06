8076fb113ae166dbd3109a13e6b87ba3bbaede26
Lennna <pampam32121@gmail.com>
2022-06-06 03:54:11 +0300

    renderCharacter function for dynamic rendering

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
 'use strict';
 
-document.getElementById('hero').innerHTML = `
-    <div class="character-card">
-        <h4 class="name"> Wizard </h4>
-        <img class="avatar" src="images/wizard.png"/>
-        <p class="health">health: <b> 60 </b></p>
-        <div class="dice-container"><div class="dice"> 6 </div></div>
-    </div>   
-`
+/*
+CHALLENGE
+1. Convert our consts into two objects called 
+"monster" and "hero".
+2. Update the renderCharacter() function so that it accepts 
+a single object "data" as its parameter instead of five string/numbers, 
+reducing the number of arguments to pass in from five to one.
+3. Update the template now each variable is coming from "data".
+4. Update the function call.
+*/
 
-document.getElementById('monster').innerHTML = `
-    <div class="character-card">
-        <h4 class="name"> Orc </h4>
-        <img class="avatar" src="images/orc.png"/>
-        <p class="health">health: <b> 10 </b></p>
-        <div class="dice-container"><div class="dice"> 4 </div></div>
-    </div>
-`
+const hero = {
+    id: 'hero',
+    name: 'Wizard',
+    avatarSrc: './images/wizard.png',
+    health: 60,
+    diceRoll: 6,
+}
+
+const monster = {
+    id: 'monster',
+    name: 'Orc',
+    avatarSrc: './images/orc.png',
+    health: 10,
+    diceRoll: 4,
+};
+
+
+function renderCharacter({id, name, avatarSrc, health, diceRoll}) {
+    document.getElementById(id).innerHTML = `
+        <div class="character-card">
+            <h4 class="name">${name}</h4>
+            <img class="avatar" src="${avatarSrc}"/>
+            <p class="health">health: <b> ${health} </b></p>
+            <div class="dice-container"><div class="dice">${diceRoll}</div></div>
+        </div>   
+    `
+}
+
+renderCharacter(hero);
+renderCharacter(monster);
