be961684d1e042711bb0e9cb398f43156e9259bc
Lennna <pampam32121@gmail.com>
2022-06-07 04:26:22 +0300

    organised code using modules

---
+++ data.js
@@ -0,0 +1,16 @@
+const characterData = {
+    hero: {
+        name: 'Wizard',
+        avatarSrc: './images/wizard.png',
+        health: 60,
+        diceCount: 3,
+    },
+    monster: {
+        name: 'Orc',
+        avatarSrc: './images/orc.png',
+        health: 10,
+        diceCount: 1,
+    }
+};
+
+export default characterData;
--- index.js
+++ index.js
@@ -1,58 +1,12 @@
-'use strict';
-
-function getDiceRollArray(diceCount) {
-    return new Array(diceCount)
-        .fill(0)
-        .map( () => Math.floor(Math.random() * 6) + 1);
-};
-
-const characterData = {
-    hero: {
-        id: 'hero',
-        name: 'Wizard',
-        avatarSrc: './images/wizard.png',
-        health: 60,
-        diceCount: 3,
-    },
-    monster: {
-        id: 'monster',
-        name: 'Orc',
-        avatarSrc: './images/orc.png',
-        health: 10,
-        diceCount: 1,
-    }
-};
-
-function Character(data) {
-    Object.assign(this, data);
-
-    this.getDiceHtml = function(diceCount) {
-        return getDiceRollArray(diceCount)
-                .map(num => `<div class="dice">${num}</div>`)
-                .join('');
-    };
-
-    this.renderCharacter = function() {
-        const { id, name, avatarSrc, health, diceCount} = this;
-        const diceHtml = this.getDiceHtml(diceCount);
-
-        return `
-            <div class="character-card">
-                <h4 class="name">${name}</h4>
-                <img class="avatar" src="${avatarSrc}"/>
-                <p class="health">health: <b> ${health} </b></p>
-                <div class="dice-container">${diceHtml}</div>
-            </div>   
-        `;
-    }
-}
+import characterData from './data.js';
+import Character from './Character.js';
 
 const wizard = new Character(characterData.hero);
 const orc = new Character(characterData.monster);
 
 function render() {
-    document.getElementById(wizard.id).innerHTML = wizard.renderCharacter();
-    document.getElementById(orc.id).innerHTML = orc.renderCharacter();
+    document.getElementById('hero').innerHTML = wizard.renderCharacter();
+    document.getElementById('monster').innerHTML = orc.renderCharacter();
 };
 
 render();
