1364484e8b5b03df740ab0f4bd01662d257bd180
Lennna <pampam32121@gmail.com>
2022-06-09 17:17:29 +0300

    added timeout when a monster changes and when the game ends, disabled attack bottom when the changes take place

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import characterData from './data.js';
 import Character from './Character.js';
 
 let monstersArray = ["orc", "demon", "goblin"];
+let isWaiting = false;
 
 function getNewMonster() {
     const nextMonsterData = characterData[monstersArray.shift()];
@@ -9,38 +10,47 @@ function getNewMonster() {
 }
 
 function attack() {
-    wizard.getDiceHtml();
-    monster.getDiceHtml();
-    wizard.takeDamage(monster.currentDiceScore);
-    monster.takeDamage(wizard.currentDiceScore);
-    render();
-    
-    if (wizard.dead) {
-        endGame();
-    } else if (monster.dead) {
-        if (monstersArray.length > 0) {
-            monster = getNewMonster();
-            render()
-        } else {
+    if (!isWaiting) {
+        wizard.getDiceHtml();
+        monster.getDiceHtml();
+        wizard.takeDamage(monster.currentDiceScore);
+        monster.takeDamage(wizard.currentDiceScore);
+        render();
+
+        if (wizard.dead) {
             endGame();
+        } else if (monster.dead) {
+            isWaiting = true;
+            if (monstersArray.length > 0) {
+                setTimeout(() => {
+                    monster = getNewMonster();
+                    render();
+                    isWaiting = false;
+                }, 1500)
+            } else {
+                endGame();
+            }
         }
     }
 };
 
 function endGame() {
+    isWaiting = true;
     const endMessage = wizard.dead && monster.dead ? 'No victors - all creatures are dead' 
         : wizard.dead ? `The ${monster.name} is Victorious` 
         : 'The Wizard Wins';
     
     const endEmoji = !wizard.dead ? "🔮" : "☠️";
 
-    document.querySelector('body').innerHTML = `
-        <div class="end-game">
-            <h2>Game Over</h2>
-            <h3>${endMessage}</h3>
-            <p class="end-emoji">${endEmoji}</p>
-        </div>
-    `;
+    setTimeout(() => {
+        document.querySelector('body').innerHTML = `
+            <div class="end-game">
+                <h2>Game Over</h2>
+                <h3>${endMessage}</h3>
+                <p class="end-emoji">${endEmoji}</p>
+            </div>
+        `;
+    }, 1500);
 };
 
 const wizard = new Character(characterData.hero);
